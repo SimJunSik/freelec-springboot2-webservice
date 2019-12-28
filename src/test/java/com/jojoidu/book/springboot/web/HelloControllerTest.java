b020715be43170638df94c5997e535540bccf022
SimJunSik <wnstlr24@naver.com>
2019-12-28 10:50:27 +0900

    수정

--- src/test/java/com/jojoidu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoidu/book/springboot/web/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.jojoidu.book.springboot.web;
 
+import com.jojoidu.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
